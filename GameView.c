ee4e75d66a7c7d7e0c1287536ecb4545d3a1cb03
z5063156 <z5063156@cse.unsw.EDU.AU>
2015-09-13 15:33:32 +1000

    Failing testGameView line 32

--- GameView.c
+++ GameView.c
@@ -1,25 +1,98 @@
 // GameView.c ... GameView ADT implementation
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include "Globals.h"
 #include "Game.h"
 #include "GameView.h"
+#include "Map.h"
 // #include "Map.h" ... if you decide to use the Map ADT
+
+#define PLAY_LENGTH 8
      
+     
+typedef struct _player {
+     int health;
+     LocationID currlocation;
+} player;
+
 struct gameView {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    int hello;
+    player players[NUM_PLAYERS];
+    //char *playermessages;
+    //char *pastplays;
+    int score;
+    int roundnum;
+    int turnnum;
 };
-     
 
 // Creates a new GameView to summarise the current state of the game
 GameView newGameView(char *pastPlays, PlayerMessage messages[])
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    GameView gameView = malloc(sizeof(struct gameView));
-    gameView->hello = 42;
-    return gameView;
+     
+    GameView g = malloc(sizeof(struct gameView));
+    int i = 0;
+    int currplay = 0;
+    //int currplayer;
+    int newlocid = 0;
+    assert(g != NULL);
+    
+    //initialize score, roundnum
+    g->score = 366;
+    g->roundnum = 0;
+    g->turnnum = 0;
+
+    //initialize hunters 
+    for(i=0; i<(NUM_PLAYERS-1); i++) {
+        g->players[i].health = GAME_START_HUNTER_LIFE_POINTS;
+        g->players[i].currlocation = UNKNOWN_LOCATION;
+    }
+        
+    //initialize dracula 
+    g->players[i].health = GAME_START_BLOOD_POINTS;
+    g->players[i].currlocation = UNKNOWN_LOCATION;
+    
+    //loop through pastPlays to create game status  
+    if (pastPlays[currplay] != '\0') {
+         do { 
+            printf("curr play: {%c} \n",pastPlays[currplay]);
+            
+            char newloc[2];
+            sprintf(newloc, "%c%c", pastPlays[currplay+1],pastPlays[currplay+2]);
+                        
+            if(strcmp(newloc,"C?") == 0) {
+                newlocid = 100;
+            } else if (strcmp(newloc,"S?") == 0) {
+                newlocid = 101;
+            } else {
+                newlocid = abbrevToID(newloc);
+            }
+             printf("%d\n",newlocid);
+            g->players[g->turnnum].currlocation = newlocid;  
+            printf("%d\n", g->players[g->turnnum].currlocation);
+           
+    
+            printf("turn %d \n",g->turnnum);
+            printf("round %d \n",g->roundnum);
+            
+            g->turnnum++;
+            if(g->turnnum > 4) { 
+                g->roundnum++; 
+                g->turnnum = 0; 
+            }
+             
+            currplay+=8;
+
+        } while(pastPlays[currplay-1] != '\0');
+        g->turnnum++;
+            if(g->turnnum > 4) { 
+                g->roundnum++; 
+                g->turnnum = 0; 
+        }
+    }
+ 
+    return g;
 }
      
      
@@ -33,39 +106,57 @@ void disposeGameView(GameView toBeDeleted)
 
 //// Functions to return simple information about the current state of the game
 
-// Get the current round
+// Print game information
+void printGame(GameView g) {
+
+    int i = 0;
+    
+    printf("*** Hunters");
+    for(i=0; i<(NUM_PLAYERS-1); i++) {
+        printf("player %d health: %d\n", i, g->players[i].health);
+        printf("player %d location: %d\n", i, g->players[i].currlocation);
+    }
+    printf("*** Dracula");
+    printf("player %d health: %d\n", i, g->players[i].health);
+    printf("player %d location: %d\n", i, g->players[i].currlocation);
+
+}
+
+// Get the current round                !! as a total number, or?
 Round getRound(GameView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    assert(currentView != NULL);
+    return (Round)(currentView->roundnum);
 }
 
 // Get the id of current player - ie whose turn is it?
 PlayerID getCurrentPlayer(GameView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    assert(currentView != NULL);
+    return (PlayerID)(currentView->turnnum);
 }
 
 // Get the current score
 int getScore(GameView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    assert(currentView != NULL);
+    return currentView->score;
 }
 
 // Get the current health points for a given player
 int getHealth(GameView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    assert(currentView != NULL);
+    assert(player < 5);
+    return currentView->players[player].health;
 }
 
 // Get the current location id of a given player
 LocationID getLocation(GameView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    assert(currentView != NULL);
+    assert(player < 5);     
+    return currentView->players[player].currlocation;
 }
 
 //// Functions that return information about the history of the game
